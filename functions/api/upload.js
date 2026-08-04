ba9451d450bf71bfe64e4d233ae541c339182a8c
Rizki Octaf <rizkioctafadilah96@gmail.com>
2026-08-04 16:48:52 +0700

    Add files via upload

--- functions/api/upload.js
+++ functions/api/upload.js
@@ -1,12 +1,15 @@
 export async function onRequestPost(context) {
   const request = context.request;
   const formData = await request.formData();
-  const file = formData.get('file'); 
+  const file = formData.get('file');
+  const isTemp = formData.get('isTemp'); // Mengambil status centang dari frontend
   
   if (!file) return new Response('Tidak ada file', { status: 400 });
   
-  // Menyimpan file beserta informasi tipe file-nya agar bisa di-view browser
-  await context.env.MY_BUCKET.put(file.name, file.stream(), {
+  // Jika file sementara, tambahkan teks "temp/" di depan nama filenya
+  const fileName = isTemp === 'true' ? `temp/${file.name}` : file.name;
+  
+  await context.env.MY_BUCKET.put(fileName, file.stream(), {
       httpMetadata: { contentType: file.type }
   });
   
